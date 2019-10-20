6ca312fe4d509566e1ce573a4c699b832be76c30
03regionkruto <54175086+03regionkruto@users.noreply.github.com>
2019-10-20 21:13:04 +0300

    Updates

---
+++ precache-manifest.cae69facd9bb77c071de6ba3281c0aa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c255d0908b8a6770d1e37ac5071a1e34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fc3a24d5f94adcc55ed",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "62151a159a389583c9be",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6fc3a24d5f94adcc55ed",
+    "url": "/static/js/2.01e0a467.chunk.js"
+  },
+  {
+    "revision": "62151a159a389583c9be",
+    "url": "/static/js/main.5c035d17.chunk.js"
+  },
+  {
+    "revision": "32b6964e781920cab92d",
+    "url": "/static/js/runtime-main.551d5801.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
