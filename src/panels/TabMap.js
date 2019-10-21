37235773895178ae317e6ac82c58645a9b87c386
Артём Бакута <artem@Artems-MacBook-Pro.local>
2019-10-22 04:26:48 +0500

    Balloons on marsk added

--- src/panels/TabMap.js
+++ src/panels/TabMap.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { YMaps, Map, Placemark, ZoomControl, GeolocationControl } from 'react-yandex-maps';
-
+import { YMaps, Map, Placemark, ZoomControl, GeolocationControl} from 'react-yandex-maps';
 
 
 export default class TabMap extends Component {
@@ -9,6 +8,20 @@ export default class TabMap extends Component {
     update: PropTypes.func.isRequired,
     go: PropTypes.func.isRequired,
   };
+
+  state = {
+    
+  }
+
+placeMark = {
+    geometry: [66.539792, 66.588339],
+    properties: {
+        hintContent: 'Это хинт',
+        balloonContent: 'Это балун'
+    },
+    modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
+    
+}
   
   render() {
     return (
@@ -19,9 +32,36 @@ export default class TabMap extends Component {
                 width={1000}
                 height={640}
                 defaultState={{ center: [66.540470, 66.707990], zoom: 12 }} >
-                <Placemark geometry={[66.539792, 66.588339]} />
-                <Placemark geometry={[66.5299819,66.6093746]} />
-                <Placemark geometry={[66.5341446,66.6407188]} />
+                <Placemark {...{
+                    geometry: [66.539792, 66.588339],
+                    properties: {
+                        hintContent: 'Это хинт',
+                        balloonContent: 'Это балун'
+                    },
+                    modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
+                  }
+                } 
+                />
+                <Placemark {...{
+                    geometry: [66.5299819,66.6093746],
+                    properties: {
+                        hintContent: 'Это хинт',
+                        balloonContent: 'Это балун'
+                    },
+                    modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
+                  }
+                } 
+                 />
+                <Placemark {...{
+                    geometry: [66.5341446,66.6407188],
+                    properties: {
+                        hintContent: 'Это хинт',
+                        balloonContent: 'Это балун'
+                    },
+                    modules: ['geoObject.addon.balloon', 'geoObject.addon.hint']
+                  }
+                } 
+                />
 
                 <ZoomControl options={{ float: 'right' }} />
               </Map>
